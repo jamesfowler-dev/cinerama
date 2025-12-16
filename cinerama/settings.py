83d053aa3547a3e05c5b9a834e048acafa9abfc7
jamesfowler-dev <james_fowler21@hotmail.com>
2025-12-16 13:00:11 +0000

    debug to false and add heroku url to allowed hosts

--- cinerama/settings.py
+++ cinerama/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['cinerama.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['cinerama-project-c41e4c3cc388.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
