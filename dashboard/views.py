00353898b397023583e8971d679545afd5551d26
AC-dev16 <amy_mb@hotmail.co.uk>
2025-12-16 13:06:44 +0000

    Add dashboard view and template for film selection and showtimes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def dashboard_view(request):
+    # Sample data - replace with actual database queries later
+    context = {
+        'new_releases': [
+            {'title': 'Latest Movie 1', 'poster': 'images/movie1.jpg'},
+            {'title': 'Latest Movie 2', 'poster': 'images/movie2.jpg'},
+            {'title': 'Latest Movie 3', 'poster': 'images/movie3.jpg'},
+        ],
+        'classic_films': [
+            {'title': 'Classic Film 1', 'poster': 'images/classic1.jpg'},
+            {'title': 'Classic Film 2', 'poster': 'images/classic2.jpg'},
+            {'title': 'Classic Film 3', 'poster': 'images/classic3.jpg'},
+        ],
+        'whats_on': [
+            {
+                'title': 'Movie Title 1',
+                'poster': 'images/movie1.jpg',
+                'showtimes': ['14:00', '17:30', '20:00', '22:30'],
+                'rating': 'PG-13',
+                'genre': 'Action, Adventure'
+            },
+            {
+                'title': 'Movie Title 2',
+                'poster': 'images/movie2.jpg',
+                'showtimes': ['15:00', '18:00', '21:00'],
+                'rating': '15',
+                'genre': 'Drama, Thriller'
+            },
+            {
+                'title': 'Movie Title 3',
+                'poster': 'images/movie3.jpg',
+                'showtimes': ['16:00', '19:00', '21:45'],
+                'rating': 'PG',
+                'genre': 'Comedy, Family'
+            },
+        ]
+    }
+    return render(request, 'booking/dashboard.html', context)
