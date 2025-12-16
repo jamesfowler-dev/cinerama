68dc48f0c4aeae36c5030fa48590cea8a90af721
Rafael Horwood sanchez <Rafaelshs@MacBook-Pro-3.local>
2025-12-16 06:42:58 +0000

    Add models, gitignore(mac and pycharm specific), add requirements, static js css and images folder

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# DO NOT MIGRATE UNTIL WE HAVE THE DASHBOARD APP, SOME MODELS WILL LIVE THERE
+# 1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! I JUST NEED YOUR ENV.PY TO USE python manage.py startapp
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # Create your models here.
+
+SCREEN_TYPE_CHOICES = ((0, "Silver Screen"), (1, "IMAX"), (2, "Cabaret"), (3, "VIP"))
+
+class User(User):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    # more to be added
+    def __str__(self):
+        return self.name
+
+class Screen(models.Model):
+    type = models.IntegerField(choices=SCREEN_TYPE_CHOICES, default=0)
+    number = models.IntegerField()
+    seats = models.IntegerField()
+# more to be added
+
+class Film(models.Model):
+    title = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    cast = models.CharField(max_length=100)
+    year = models.DateFieldField()
+    # more to be added
